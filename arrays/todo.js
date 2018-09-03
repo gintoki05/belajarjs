8dff154663ed26736399e6bf14a91990315e877a
gintoki05 <ajieprastyo@gmail.com>
2018-09-03 07:46:03 +0700

    todo array dasar

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['Tidur', 'Makan', 'Mandi', 'Sholat', 'Belajar'];
+
+console.log(`You have ${todos.length} todos`);
+console.log(`Todos: ${todos[0]}`);
+console.log(`Todos: ${todos[todos.length - 2]}`);
