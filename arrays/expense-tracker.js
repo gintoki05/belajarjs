e0915c51b75e7cb63ee634b4a81e976ffece319b
gintoki05 <ajieprastyo@gmail.com>
2018-09-04 13:05:26 +0700

    section array completed

---
+++ arrays/expense-tracker.js
@@ -0,0 +1,40 @@
+const account = {
+  name: 'Ajie',
+  expenses: [],
+  income: [],
+  addExpenses: function(description, amount) {
+    this.expenses.push({
+      description: description,
+      amount: amount
+    });
+  },
+  addIncome: function(description, amount) {
+    this.income.push({
+      description: description,
+      amount: amount
+    });
+  },
+  getAccountSummary: function() {
+    let totalExpenses = 0;
+    let totalIncome = 0;
+    let accountBalance = 0;
+
+    this.expenses.forEach(function(expense) {
+      totalExpenses = totalExpenses + expense.amount;
+    });
+
+    this.income.forEach(function(income) {
+      totalIncome = totalIncome + income.amount;
+    });
+
+    accountBalance = totalIncome - totalExpenses;
+
+    return `${
+      this.name
+    } has a balanace of ${accountBalance}. ${totalIncome} in income. ${totalExpenses} in expenses`;
+  }
+};
+
+account.addExpenses('Coffe', 2);
+account.addIncome('Job', 1000);
+console.log(account.getAccountSummary());
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,58 @@
-const todos = ['Tidur', 'Makan', 'Mandi', 'Sholat', 'Belajar'];
+const todos = [
+  {
+    title: 'b',
+    completed: true
+  },
+  {
+    title: 'c',
+    completed: false
+  },
+  {
+    title: 'd',
+    completed: true
+  },
+  {
+    title: 'a',
+    completed: false
+  },
+  {
+    title: 'e',
+    completed: true
+  }
+];
 
-console.log(`You have ${todos.length} todos`);
-console.log(`Todos: ${todos[0]}`);
-console.log(`Todos: ${todos[todos.length - 2]}`);
+const sortTodos = function(todos) {
+  todos.sort(function(a, b) {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      return -1;
+    } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+sortTodos(todos);
+console.log(todos);
+
+// const getThingsToDo = function(todos) {
+//   return todos.filter(function(todo) {
+//     return !todo.completed;
+//   });
+// };
+
+// console.log(getThingsToDo(todos));
+
+// const deleteTodo = function(todos, todoText) {
+//   const index = todos.findIndex(function(todo) {
+//     return todo.title.toLowerCase() === todoText.toLowerCase();
+//   });
+
+//   if (index > -1) {
+//     todos.splice(index, 1);
+//   }
+// };
+
+// deleteTodo(todos, 'Belajar');
+// console.log(todos);
